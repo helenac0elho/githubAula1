f3e2cfa8c32eb1082f0e18718ef4d1eced780c45
Helena <helena.souza1@alunos.sc.senac.br>
2022-08-26 20:33:46 -0300

    fiz o hello world

---
+++ src/githubaula01/GithubAula01.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubaula01;
+//import java.util.Scanner;
+
+/**
+ *
+ * @author helena.souza1
+ */
+public class GithubAula01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
